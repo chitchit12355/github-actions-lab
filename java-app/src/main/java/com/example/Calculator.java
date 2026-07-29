ec56d1bad435753bc05ac43fcf62b70f90f019fa
NGUYEN THI DIEM MY <phamtriet8612@gmail.com>
2026-07-29 13:55:35 +0700

    Update Calculator.java

--- java-app/src/main/java/com/example/Calculator.java
+++ java-app/src/main/java/com/example/Calculator.java
@@ -1,1 +1,11 @@
-src/main/java/com/example/Calculator.java
+package com.example;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+}
